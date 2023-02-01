45fae402f1f44feca0820adecaadbf5c89f1f86d
alekss-cv <91898963+alekss-cv@users.noreply.github.com>
2023-02-02 01:42:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+let test = document.querySelector(".test")
+const XHR = new XMLHttpRequest();
+XHR.open("GET" , "http://localhost:8848/" ,true);
+XHR.onload = ()=>{
+    console.log(XHR.status ,XHR.statusText);
+    test.innerHTML = XHR.response
+}
+XHR.send()
