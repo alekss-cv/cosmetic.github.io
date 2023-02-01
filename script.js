e98ccddc4e9bd14710b12d68f3a04beec78c9c22
alekss-cv <91898963+alekss-cv@users.noreply.github.com>
2023-02-02 01:46:30 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let test = document.querySelector(".test")
-const XHR = new XMLHttpRequest();
-XHR.open("GET" , "http://localhost:8848/" ,true);
-XHR.onload = ()=>{
-    console.log(XHR.status ,XHR.statusText);
-    test.innerHTML = XHR.response
-}
-XHR.send()
